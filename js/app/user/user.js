46e2371b7583010edaf7107417f0012e5675f14f
= <=>
2017-11-06 11:01:11 +0900

    Create User component

--- js/app/user/user.js
+++ js/app/user/user.js
@@ -3,27 +3,33 @@ function User({name = "김동빈", money = 10000} = {}) {
 		buildCoinButton(value) {
 			var button = document.createElement("button");
 
-			button.className = "button money-button coin-button ${value}"
+			button.className = `button money-button coin-button ${value}`
 			button.textContent = value;
+
+			return button;
 		},
 
 		buildBillButton(value) {
 			var button = document.createElement("button");
 
-			button.className = "button money-button bill-button ${value}"
+			button.className = `button money-button bill-button ${value}`
 			button.textContent = value;
+
+			return button;
 		},
 
 		buildMyMoneyContainer() {
 			var p = document.createElement("p");
 			var span = document.createElement("span");
 
-			p.textContent: "내 돈: ";
+			p.textContent = "내 돈: ";
 			span.className = "my-money";
 			span.id = "myMoney";
 			span.textContent = money;
 
-			p.insertAdjacentHTML("beforeend", span);
+			p.appendChild(span);
+
+			return p;
 		}
 	}
 
@@ -37,15 +43,31 @@ function User({name = "김동빈", money = 10000} = {}) {
 
 	this.purchasedProductList = [];
 
-	this.fiftyCoinButton = userDOMBuildFuncs.buildCoinButton("fifty");
-	this.oneHundredCoinButton = userDOMBuildFuncs.buildCoinButton("onehundred");
-	this.fiveHundredCoinButton = userDOMBuildFuncs.buildCoinButton("fivehundred");
-	this.oneThousandBillButton = userDOMBuildFuncs.buildBillButton("onethousand");
+	this.moneyButtonList = {
+		"fiftyCoinButton": userDOMBuildFuncs.buildCoinButton("fifty"),
+		"oneHundredCoinButton": userDOMBuildFuncs.buildCoinButton("onehundred"),
+		"fiveHundredCoinButton": userDOMBuildFuncs.buildCoinButton("fivehundred"),
+		"oneThousandBillButton": userDOMBuildFuncs.buildBillButton("onethousand"),
+	};
 	this.myMoneyContainer = userDOMBuildFuncs.buildMyMoneyContainer();
 }
 
 User.prototype.init = function() {
-	var moneyWrapper = getWrapperAround("money-wrapper");
-	var moneyListWrapper = getWrapperAround("money-list-wrapper");
-	var myMoneyContainerWrapper = getWrapperAround("mymoney-container-wrapper");
+	var userWrapper = dom.getWrapperAround("user-wrapper");
+	var moneyListWrapper = dom.getWrapperAround("money-list-wrapper");
+	var myMoneyContainerWrapper = dom.getWrapperAround("mymoney-container-wrapper");
+
+	for (var moneyButton in this.moneyButtonList) {
+		var moneyWrapper = dom.getWrapperAround("money-wrapper");
+
+		moneyWrapper.appendChild(this.moneyButtonList[moneyButton]);
+		moneyListWrapper.appendChild(moneyWrapper);
+	}
+
+	myMoneyContainerWrapper.appendChild(this.myMoneyContainer);
+
+	userWrapper.appendChild(moneyListWrapper);
+	userWrapper.appendChild(myMoneyContainerWrapper);
+
+	dom.root.appendChild(userWrapper);
 };
